b7988aac46d7762b1206f4b81bfabab57e0e8d58
wxchen7 <79951404+wxchen7@users.noreply.github.com>
2022-11-01 20:24:15 +0800

    Add files via upload

---
+++ airbnb/static/js/461.1fc19513.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkairbnb=self.webpackChunkairbnb||[]).push([[461],{461:function(e,n,i){i.r(n);var r=i(2791),u=i(184),a=(0,r.memo)((function(){return(0,u.jsx)("div",{children:"Detail"})}));n.default=a}}]);
+//# sourceMappingURL=461.1fc19513.chunk.js.map
